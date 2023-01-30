fbb53deb3dce958c7cb32badc0b2992e06121666
kuzafkto <kuzafkto@gmail.com>
2023-01-30 12:52:51 +0100

    Ejercicio1

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -3,5 +3,14 @@ import java.util.ArrayList;
 public class Ejercicio1 {
     public static void main(String[] args) {
         ArrayList<String>names=new ArrayList<>();
+        names.add("Adrian");
+        names.add("Jesus");
+        names.add("Denisa");
+        names.add("Pablo");
+        names.add("Javier");
+        names.add("Nuria");
+        for (String string : names) {
+           System.out.println(string); 
+        }
     }
 }
